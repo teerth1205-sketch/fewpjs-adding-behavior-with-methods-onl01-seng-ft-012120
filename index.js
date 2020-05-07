138e5cc33fd2283a192fe52770c52e8f7b917c09
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-07 21:30:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    
+    function speak() {
+        return `${this.name} says meow!`
+    }
+}
