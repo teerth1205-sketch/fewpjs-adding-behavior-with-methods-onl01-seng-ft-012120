7034c870857a261cf893341975a87e1520ba1fb9
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-07 21:37:26 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,36 @@ class Cat {
         this.sex = sex
     }
     
-    function speak() {
+     speak () {
         return `${this.name} says meow!`
+    }
+}
+
+// Your code here
+class Dog {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    
+     speak () {
+        return `${this.name} says woof!`
+    }
+}
+
+// Your code here
+class Bird {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    
+     speak () {
+         if (this.sex == "male"){
+            return `It's me! ${this.name}, the parrot!`
+         }else {
+             return `${this.name} says squawk!`
+         }
+       
     }
 }
